292cb682d6788783a0c3036744e01992423aefa8
Mohammed Shajahan <mohammedsh6236@gmail.com>
2023-07-18 17:48:55 +0530

    setup:

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 import { defineConfig } from 'astro/config';
+import vercelEdge from '@astrojs/vercel/edge';
 
 import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx()]
+  integrations: [mdx()],
+  output: 'server',
+  adapter: vercelEdge(),
 });

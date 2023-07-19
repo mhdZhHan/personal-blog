5177f6632ea801fe4c3f2a10e3512e2484c77a57
Mohammed Shajahan <mohammedsh6236@gmail.com>
2023-07-19 19:42:19 +0530

    Add cli for creating new post

---
+++ scripts/createMdx.js
@@ -0,0 +1,45 @@
+import fs from "fs"
+import path from "path"
+import { fileURLToPath } from "url"
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+const createMdxFile = (fileName) => {
+    const projectRoot = path.join(__dirname, '..');
+    const postsDir = path.join(projectRoot, "src", "pages", "blogs")
+    const currentDate = new Date().toISOString().slice(0, 10)
+    const filePath = path.join(postsDir, `${currentDate}-${fileName}.mdx`)
+
+    // default mdx content 
+    const markdownContent = `---
+layout: "../../layouts/BlogPostLayout.astro"
+title: ${fileName}
+draft: false
+pubDate: ${currentDate}
+description: "description..."
+author: "Mohammed"
+image: "/images/posts/"
+tags: []
+---
+
+# ${fileName}
+
+Post content here...
+`
+
+    fs.writeFile(filePath, markdownContent, (err) => {
+        if (err) {
+            console.error("Error creating post file:", err);
+        } else {
+            console.log(`Post '${filePath}' created successfully!`)
+        }
+    })
+}
+
+const [, , fileName] = process.argv;
+if (!fileName) {
+    console.error("Please provide a valid filename.")
+} else {
+    createMdxFile(fileName)
+}
--- src/scripts/app.js
+++
@@ -1,9 +0,0 @@
-document.querySelectorAll(".input-box").forEach((element) => {
-    element.addEventListener("blur", (event) => {
-        if (event.target.value != "") {
-            event.target.nextElementSibling.classList.add("filled")
-        } else {
-            event.target.nextElementSibling.classList.remove("filled")
-        }
-    })
-})

7b90eebfd96661f7f93a788f3c1d5854b04827e4
Mohammed Shajahan <mohammedsh6236@gmail.com>
2023-07-19 16:35:41 +0530

    idk

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,15 @@
 import { defineConfig } from 'astro/config'
 
 import mdx from "@astrojs/mdx"
+import remarkToc from 'remark-toc';
 import vercelEdge from '@astrojs/vercel/edge'
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [mdx({
     syntaxHighlight: 'shiki',
-    shikiConfig: { theme: 'nord' },
+    shikiConfig: { theme: 'nord', wrap: true, langs: [] },
+    remarkPlugins: [ [remarkToc, { heading: "contents"} ] ],
   })],
   output: 'server',
   adapter: vercelEdge(),

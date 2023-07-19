6f0fab82664a0b1dd0699ca40f1142c0416b4645
Mohammed Shajahan <mohammedsh6236@gmail.com>
2023-07-19 17:52:51 +0530

    idk commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,9 @@ import { defineConfig } from "astro/config"
 import mdx from "@astrojs/mdx"
 // import markdownConfig from "./markdown.config"
 
-// https://astro.build/config
 export default defineConfig({
     integrations: [
         mdx({
-            // ...markdownConfig,
             syntaxHighlight: "shiki",
             shikiConfig: { theme: "slack-dark", wrap: true, langs: [] },
         }),

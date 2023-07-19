b6a2b0a8ba98fe82dd644e20a56add36c74f0006
Mohammed Shajahan <mohammedsh6236@gmail.com>
2023-07-19 17:43:47 +0530

    trying to add toc

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
-import { defineConfig } from 'astro/config'
-
+import { defineConfig } from "astro/config"
 import mdx from "@astrojs/mdx"
+// import markdownConfig from "./markdown.config"
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx({
-    syntaxHighlight: 'shiki',
-    shikiConfig: { theme: 'slack-dark', wrap: true, langs: [] },
-  })],
+    integrations: [
+        mdx({
+            // ...markdownConfig,
+            syntaxHighlight: "shiki",
+            shikiConfig: { theme: "slack-dark", wrap: true, langs: [] },
+        }),
+    ],
 })

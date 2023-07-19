1209b6be2c7111f5f42552abadc10fdc54a5c727
Mohammed Shajahan <mohammedsh6236@gmail.com>
2023-07-19 17:37:51 +0530

    remove something add ...

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,11 @@
 import { defineConfig } from 'astro/config'
 
 import mdx from "@astrojs/mdx"
-import remarkToc from 'remark-toc';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [mdx({
     syntaxHighlight: 'shiki',
     shikiConfig: { theme: 'slack-dark', wrap: true, langs: [] },
-    remarkPlugins: [ [remarkToc, { heading: "contents"} ] ],
   })],
 })

6d58fc203f7ce8f6bae2072af938eae302d2cf55
Mohammed Shajahan <mohammedsh6236@gmail.com>
2023-07-21 12:20:17 +0530

    Nice

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,20 @@
-import { defineConfig } from "astro/config"
-import mdx from "@astrojs/mdx"
-import react from "@astrojs/react"
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import react from "@astrojs/react";
 
 // import markdownConfig from "./markdown.config"
-import tokyoNight from "./tokyo-night.json"
+import tokyoNight from "./tokyo-night.json";
+
+import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [
-        mdx({
-            syntaxHighlight: "shiki",
-            shikiConfig: {
-                theme: tokyoNight,
-                wrap: true,
-                langs: [],
-            },
-        }),
-        react(),
-    ],
-})
+  integrations: [mdx({
+    syntaxHighlight: "shiki",
+    shikiConfig: {
+      theme: tokyoNight,
+      wrap: true,
+      langs: []
+    }
+  }), react(), image()]
+});

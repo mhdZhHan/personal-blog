7a696c6f6eff7630bd8ea6a723ecc50c508fde10
Mohammed Shajahan <mohammedsh6236@gmail.com>
2023-07-23 18:49:19 +0530

    search functionality error

--- src/pages/search.json.js
+++ src/pages/search.json.js
@@ -1,19 +1,24 @@
-// import { getCollection } from "astro:content"
-// import { sortByDate, nonDraftPosts } from '../utils'
+import { getCollection } from "astro:content"
+import { sortByDate, nonDraftPosts } from '../utils'
 
-// async function getPosts() {
-//     const posts = (await getCollection("blogs"))
-//     const nonDraftPosts_ = nonDraftPosts(posts)
-//     const sortedPosts = sortByDate(nonDraftPosts_)
+async function getPosts() {
+    const posts = (await getCollection("blog"))
+    const nonDraftPosts_ = nonDraftPosts(posts)
+    const sortedPosts = sortByDate(nonDraftPosts_)
 
-//     return sortedPosts
-// }
+    return sortedPosts.map((post) => ({
+        slug: post.slug,
+        title: post.data.title,
+        description: post.data.description,
+        pubDate: post.data.pubDate,
+    }))
+}
 
-// export async function get({}) {
-//     return new Response(JSON.stringify(await getPosts()), {
-//         status: 200,
-//         headers: {
-//             "Content-Type": "application/json"
-//         }
-//     })
-// }
+export async function get({}) {
+    return new Response(JSON.stringify(await getPosts()), {
+        status: 200,
+        headers: {
+            "Content-Type": "application/json"
+        }
+    })
+}

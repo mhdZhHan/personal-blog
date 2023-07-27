835fd34760ef5dddd70ca79d89d6c9936cda5974
Mohammed Shajahan <mohammedsh6236@gmail.com>
2023-07-27 11:06:47 +0530

    light mode initial setup

---
+++ src/scripts/theme.js
@@ -0,0 +1,33 @@
+window.addEventListener('load', ()=>{
+    let darkMode = localStorage.getItem('light')
+    
+    const darkModeToggle = document.querySelector('#toggle_theme')
+
+    let confirmText = "light mode is currently functional, but we're not satisfied with its performance just yet"
+
+    const enableLightMode = ()=>{
+        if (confirm(confirmText)) {
+            document.body.classList.add('light')
+            localStorage.setItem('light', 'enabled')
+        }
+    }
+
+    const disableLightMode = () => {
+        document.body.classList.remove('light')
+        localStorage.setItem('light', null)
+    }
+
+    if (darkMode === 'enabled') {
+        enableLightMode()
+    }
+
+    darkModeToggle.addEventListener('click', () => {
+        darkMode = localStorage.getItem('light')
+        
+        if (darkMode !== 'enabled') {
+            enableLightMode()
+        } else {  
+            disableLightMode()
+        }
+    })
+})

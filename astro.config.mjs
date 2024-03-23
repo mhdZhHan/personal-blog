25f970ab6f8adcc1afd89fe6cbdac7d73e12f986
Mohammed Shajahan <dev.mohammedsh@gmail.com>
2024-03-24 00:44:00 +0530

    Add syntax highlight

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,17 @@
 import { defineConfig } from "astro/config"
 import mdx from "@astrojs/mdx"
 import sitemap from "@astrojs/sitemap"
-import robotsTxt from 'astro-robots-txt';
+import robotsTxt from "astro-robots-txt"
+
+import rehypePrism from "rehype-prism-plus"
+import rehypeCodeTitles from "rehype-code-titles"
 
 export default defineConfig({
 	site: "https://mohammedsh.xyz",
 	integrations: [
 		mdx({
 			syntaxHighlight: false,
+			rehypePlugins: [rehypeCodeTitles, rehypePrism],
 		}),
 		sitemap(),
 		robotsTxt(),

428374ba2dc97d2cb026f418b3a78657e1bbb606
Mohammed Shajahan <dev.mohammedsh@gmail.com>
2024-04-05 00:13:57 +0530

    new updates && new tries

--- src/utils/AppConfig.js
+++
@@ -1,8 +0,0 @@
-export const AppConfig = {
-    site_name: "Mohammed's Blog",
-    title: "Mohammed's Blog",
-    description: "Official blog",
-    author: "Mohammed",
-    locale_region: "en-us",
-    locale: "en",
-};

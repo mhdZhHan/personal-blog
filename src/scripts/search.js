784cbb174a761fd9b297d4a19a087bb955b14beb
Mohammed Shajahan <dev.mohammedsh@gmail.com>
2024-04-07 14:15:41 +0530

    Add auth ui && fix search ui

--- src/scripts/search.js
+++ src/scripts/search.js
@@ -96,12 +96,12 @@ async function requestSearch() {
 
                     <div class="meta">
                         <div class="date">
-                            <Calendar class="shrink-0" size="{14}" aria-hidden />
+                            <svg xmlns="http://www.w3.org/2000/svg" stroke-width="2" width="14" height="14" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" fill="none" viewBox="0 0 24 24" class="shrink-0" aria-hidden="true" data-astro-source-loc="23:17">  <path d="M8 2v4" data-astro-source-loc="7:3"></path> <path d="M16 2v4" data-astro-source-loc="8:3"></path> <rect width="18" height="18" x="3" y="4" rx="2" data-astro-source-loc="9:3"></rect> <path d="M3 10h18" data-astro-source-loc="10:3"></path>  </svg>
                             <time datetime="${pubDate}">${simplifyDate(pubDate).simplifiedDate}</time>
                         </div>
 
                         <div class="read">
-                            <Clock4 class="shrink-0" size="{14}" aria-hidden />
+                            <svg xmlns="http://www.w3.org/2000/svg" stroke-width="2" width="14" height="14" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" fill="none" viewBox="0 0 24 24" class="shrink-0" aria-hidden="true" data-astro-source-loc="23:17">  <circle cx="12" cy="12" r="10"  data-astro-source-loc="7:3"></circle> <polyline points="12 6 12 12 16 14"  data-astro-source-loc="8:3"></polyline>  </svg>
                             <span>${readingTime} Min Read</span>
                         </div>
                     </div>

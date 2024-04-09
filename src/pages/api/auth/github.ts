945f6bd81e2408e30238795d39d0e9b6b252100f
Mohammed Shajahan <dev.mohammedsh@gmail.com>
2024-04-09 18:47:56 +0530

    Add firebase keys in .env

--- src/pages/api/auth/github.ts
+++ src/pages/api/auth/github.ts
@@ -8,9 +8,21 @@ import { initializeApp, cert, getApps } from "firebase-admin/app"
 
 import { getAuth } from "firebase-admin/auth"
 
-import serviceAccount from "../../../../personal-blog-firebase-key.json"
-
 const activeApps = getApps()
+
+const serviceAccount = {
+	type: "service_account",
+	project_id: import.meta.env.FIREBASE_PROJECT_ID,
+	private_key_id: import.meta.env.FIREBASE_PRIVATE_KEY_ID,
+	private_key: import.meta.env.FIREBASE_PRIVATE_KEY,
+	client_email: import.meta.env.FIREBASE_CLIENT_EMAIL,
+	client_id: import.meta.env.FIREBASE_CLIENT_ID,
+	auth_uri: import.meta.env.FIREBASE_AUTH_URI,
+	token_uri: import.meta.env.FIREBASE_TOKEN_URI,
+	auth_provider_x509_cert_url: import.meta.env.FIREBASE_AUTH_CERT_URL,
+	client_x509_cert_url: import.meta.env.FIREBASE_CLIENT_CERT_URL,
+	universe_domain: "googleapis.com",
+}
 
 export const app =
 	activeApps.length === 0

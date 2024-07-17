5dea4f91346b07a2ffd2b452e9c6ea76563116fb
Mohammed Shajahan <dev.mohammedsh@gmail.com>
2024-07-18 01:14:32 +0530

    Middleware route protection

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,8 +4,25 @@ import { defineMiddleware } from "astro:middleware"
 export const onRequest = defineMiddleware((context, next) => {
 	const currentUser = auth.currentUser
 
+	const { pathname } = context.url
+
+	if (
+		!currentUser &&
+		pathname === "/auth/user" &&
+		context.request.method === "GET"
+	) {
+		return context.redirect("/auth/login")
+	}
+
 	if (currentUser) {
 		context.locals.userEmail = currentUser.email
+	}
+
+	if (
+		currentUser &&
+		(pathname === "/auth/login" || pathname === "/auth/signup")
+	) {
+		return context.redirect("/")
 	}
 
 	return next()

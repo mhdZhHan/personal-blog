d62aac87045a85dd187ff226b2a7bd507c3857f7
Mohammed Shajahan <dev.mohammedsh@gmail.com>
2024-07-17 22:29:30 +0530

    Type based form submission (login, signup)

--- src/scripts/auth.ts
+++ src/scripts/auth.ts
@@ -1,11 +1,28 @@
-import { actions } from "astro:actions"
+import { actions, isInputError } from "astro:actions"
 
 const authForm = document.querySelector("#authForm") as HTMLFormElement
 
 authForm?.addEventListener("submit", async (event) => {
-    event.preventDefault()
-    const formData = new FormData(authForm)
-    console.log(formData)
-    // passing the form data to createAccount action
-    await actions.createAccount(formData)
+	event.preventDefault()
+	const formData = new FormData(authForm)
+	const formType = formData.get("formType") as string
+
+	console.log(formData)
+
+	if (formType === "Signup") {
+		// passing the form data to createAccount action
+		const { error, data } = await actions.createAccount.safe(formData)
+
+		if (error) {
+			// handle error (future)
+			console.log(error)
+			if (isInputError(error)) {
+				console.log(error.fields)
+			}
+			return
+		}
+
+		window.location.reload()
+	} else if (formType === "Sign In") {
+	}
 })

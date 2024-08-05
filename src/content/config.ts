ab002ae008e467c9d4cadb8590ec37746d181152
Mohammed Shajahan <dev.mohammedsh@gmail.com>
2024-08-05 11:18:56 +0530

    ✏️ Demos Basic setup

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,15 +1,29 @@
-import { z, defineCollection } from 'astro:content'
+import { z, defineCollection } from "astro:content"
 
-const blog = defineCollection({
-    schema: z.object({
-        title: z.string(),
-        draft: z.boolean().default(false),
-        pubDate: z.date(),
-        description: z.string(),
-        author: z.enum(["Mohammed"]),
-        image: z.string().optional(),
-        tags: z.array(z.string()),
-    }),
+const blogCollection = defineCollection({
+	schema: z.object({
+		title: z.string(),
+		draft: z.boolean().default(false),
+		pubDate: z.date(),
+		description: z.string(),
+		author: z.enum(["Mohammed"]),
+		image: z.string().optional(),
+		tags: z.array(z.string()),
+	}),
 })
 
-export const collections = { blog }
+const demosCollection = defineCollection({
+	schema: z.object({
+		title: z.string(),
+		description: z.string(),
+		pubDate: z.date(),
+		updatedDate: z.date().optional(),
+		image: z.string().optional(),
+		category: z.enum(["App", "Web", "AI", "Desktop"]),
+		tags: z.array(z.string()).optional(),
+		liveDemo: z.string().url().optional(),
+		sourceCode: z.string().url().optional(),
+	}),
+})
+
+export const collections = { blog: blogCollection, demos: demosCollection }
